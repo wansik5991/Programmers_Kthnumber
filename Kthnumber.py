707aa7f795190ca9be4b80ef1678c3341ae46825
wansik5991 <kevin103@daum.net>
2021-09-07 05:40:27 +0900

    Programmers_Kthnumber
    프로그래머스_K번째수

---
+++ Kthnumber.py
@@ -0,0 +1,9 @@
+import os
+def solution(array, commands):
+    answer = []
+    for command in commands :
+        answer.append(sorted(array[command[0]-1 : command[1]])[command[2]-1])
+    return answer
+
+os.system('cls')
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
